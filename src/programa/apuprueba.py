ee956c0197b313e5ac01530c2d92d47dc07fd5e6
Ivan Gonzalez <ivan_gonzalez@ciencias.unam.mx>
2025-11-13 14:01:52 -0600

    solo archivo necesario lol owo

--- src/programa/apuprueba.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-request=input("que quieres preguntar?")
-client = genai.Client(api_key="AIzaSyA_7BCTj1dqnBmEEMTBi_FUq7qe8c03Dgk")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=request
-)
-print(response.text)
